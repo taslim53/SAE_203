1c69f0e26f557dcf4c14b867a1ab6a489448de33
Zeloiz <167115280+Zeloiz@users.noreply.github.com>
2024-04-16 11:24:02 +0200

    zsz

--- SAE_203/coursPHP/profil.php
+++ SAE_203/coursPHP/profil.php
@@ -6,20 +6,22 @@
 	header("Content-Type: text/html; charset=utf-8") ;
 	 
 	if(isset($_SESSION['id_membre']) AND $_SESSION['id_membre'] > 0) {
+		unset($_SESSION["indice_question_derniere_reponse"]) ; // Redémarrer au début du quiz
 ?><!DOCTYPE html>
 <html lang="fr">
 	<head>
 		<title>TUTO PHP</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" href="styles.css" type="text/css" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+		<link rel="stylesheet" href="styles.css" type="text/css" >
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 	</head>
 	<body>
 		<main>
-			<h1>Profil de <?php echo($_SESSION["pseudo"]); ?></h1>
-			<div class="colspan-2">
-				<p>Pseudo = <?php echo($_SESSION["pseudo"]); ?></p>
-				<p>Mail = <?php echo($_SESSION["mail"]); ?></p>
+			<h1>Profil de <?php echo($_SESSION['pseudo']); ?></h1>
+			<div class="profil">
+				<p>Pseudo = <?php echo ($_SESSION['pseudo']); ?></p>
+				<p>Mail = <?php echo ($_SESSION['mail']); ?></p>
+				<a href="quiz.php">Faire le quiz</a>
 				<a href="editionprofil.php">Editer mon profil</a>
 				<a href="deconnexion.php">Se déconnecter</a>
 			</div>
@@ -27,7 +29,8 @@
 	</body>
 </html>
 <?php   
-	} else {
-		header("Location: connexion.php");
+	}
+	else {
+		header("Location: connexion.php") ;
 	}
 ?>
