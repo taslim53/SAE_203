b929393d55dd5fd70c46f1923739efb71ccbd892
Zeloiz <167115280+Zeloiz@users.noreply.github.com>
2024-04-16 12:10:17 +0200

    test

---
+++ SAE_203/html/pageArabe.php
@@ -0,0 +1,14 @@
+<?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+	session_start();
+	header("Content-Type: text/html; charset=utf-8") ;
+	
+	require (__DIR__ . "/param.inc.php");
+	
+	require (__DIR__ . "/src/Membres/Administrer.php");
+	
+	$administrerMembres = new Membres\Administrer(MYHOST, MYDB, MYUSER, MYPASS) ; 
+
+    //coller partie galerie du portfolio (csv avec $tab 1, 2, 3...)
